4f6ca50c339e59001f902bcefd4e09b21d7f55f0
rollingrock <rollingrock16@gmail.com>
2024-04-15 16:56:47 -0500

    Adding initial project files from SkyrimVRESL

---
+++ cmake/headerlist.cmake
@@ -0,0 +1,15 @@
+set(headers ${headers}
+	src/DataHandler.h
+	src/SkyrimVRESLAPI.h
+	src/PCH.h
+	src/hooks.h
+	src/tesfilehooks.h
+	src/saveloadhooks.h
+	src/sksevrhooks.h
+	src/startuphooks.h
+	src/eslhooks.h
+	src/SaveLoadGame.h
+	src/Settings.h
+	src/Util.h
+	src/Papyrus.h
+)
---
+++ cmake/sourcelist.cmake
@@ -0,0 +1,9 @@
+set(sources ${sources}
+	src/DataHandler.cpp
+	src/SkyrimVRESLAPI.cpp
+	src/SaveLoadGame.cpp
+	src/main.cpp
+	src/PCH.cpp
+	src/Settings.cpp
+	src/Papyrus.cpp
+)
